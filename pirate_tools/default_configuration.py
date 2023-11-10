8db8931f899f59670e4f87024442121e933beb1d
Eric Crowley <eecmtf@gmail.com>
2023-11-10 09:17:34 -0700

    plus default applicaiton directory

--- pirate_tools/default_configuration.py
+++ pirate_tools/default_configuration.py
@@ -1,5 +1,19 @@
+import pathlib
+
 
 class ApplicationConfiguration:
     """
     Object containing the default or modified configuration state for the application
     """
+
+    CONFIG_DIR_NAME = ".pirate-tools"
+
+    def __init__(self, local_configuration_directory: str = None) -> None:
+        self._local_configuration_directory = local_configuration_directory
+
+    @property
+    def local_configuration_directory(self) -> str:
+        if not self._local_configuration_directory:
+            config_dir_path = pathlib.Path.home() + self.CONFIG_DIR_NAME
+            self._local_configuration_directory = config_dir_path
+        return self._local_configuration_directory
