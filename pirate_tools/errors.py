d1a8469583e4cc360921372c6265321f8e4ccd91
Eric Crowley <eecmtf@gmail.com>
2023-11-12 08:09:01 -0700

    plus module for errors specific to the application

---
+++ pirate_tools/errors.py
@@ -0,0 +1,12 @@
+class NoConfigFileFoundError(Exception):
+    """
+    Exception should be raised when no configuration file is found
+    """
+
+
+class MalformedConfigFileError(Exception):
+    """
+    Exception should be raised when the configuration file can not be completely parsed.
+    This error includes keys being missing from the configuration file, or invalid 
+    values existing in the configuration file
+    """
